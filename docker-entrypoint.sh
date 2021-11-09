82afddeab9c5ecfde8b6b2f8edcfd608b9643472
Ken Graf <ken.graf@cs.unh.edu>
2021-11-09 10:05:46 -0500

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,13 +18,15 @@ if [ ! -f .ctfd_secret_key ] && [ -z "$SECRET_KEY" ]; then
     fi
 fi
 
+python ping.py
+
 # Initialize database
 python manage.py db upgrade
 
 # Start CTFd
 echo "Starting CTFd"
 exec gunicorn 'CTFd:create_app()' \
-    --bind '0.0.0.0:8000' \
+    --bind '0.0.0.0:80' \
     --workers $WORKERS \
     --worker-tmp-dir "$WORKER_TEMP_DIR" \
     --worker-class "$WORKER_CLASS" \
